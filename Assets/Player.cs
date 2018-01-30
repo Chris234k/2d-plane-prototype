e9dac4d4b2fcae71b4c4c7fa70669cec68941f81
Chris Bruno <chris234k@gmail.com>
2018-01-29 23:36:09 -0500

    Some comments

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Player physics
     float max_vel = 1.5f;
     Vector2 accel, vel;
     Vector2 gravity = new Vector2(0, -2f);
     float friction = 0.95f;
 
-    int thruster_anim_index = 0;
-
+    // Movement Animations
+    public ParticleSystem left_wing_brake, right_wing_brake;
+    public TrailRenderer left_wing_trail, right_wing_trail;
+    
+    // Shooting
     public GameObject bullet;
     public Transform bullet_spawn;
 
-    public ParticleSystem left_wing_brake, right_wing_brake;
-    public TrailRenderer left_wing_trail, right_wing_trail;
-
+    // Thruster animations
+    int thruster_anim_index = 0;
     public SpriteRenderer thruster;
     public Color[] thruster_colors; // 0th element is base color
 
+    // Screen wrapping
     Vector2 cam_tl, cam_br;
-
     float player_width = 3.5f;
 
     void Start()
@@ -36,11 +39,11 @@ public class Player : MonoBehaviour
     //
     void Update()
     {
-        float raw_hor = Input.GetAxisRaw("Horizontal");
-        float raw_vert = Input.GetAxisRaw("Vertical");
+        float raw_h = Input.GetAxisRaw("Horizontal");
+        float raw_v = Input.GetAxisRaw("Vertical");
 
-        float hor = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
         float thrust = Input.GetAxis("Thrust");
         float brake = Input.GetAxis("Brake");
@@ -55,15 +58,15 @@ public class Player : MonoBehaviour
             right_wing_brake.Emit(1);
         }
 
-        if ( raw_hor != 0 || raw_vert != 0 ) // Rotate towards player input (don't rotate without input)
+        if ( raw_h != 0 || raw_v != 0 ) // Rotate towards player input (don't rotate without input)
         {
-            transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(vert, hor) * Mathf.Rad2Deg);
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(v, h) * Mathf.Rad2Deg);
         }
         else
         {
             // Use forward direction of the plane as the dir to apply thrust towards
-            hor = Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad);
-            vert = Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad);
+            h = Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad);
+            v = Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad);
         }
 
         if ( thrust > 0 )
@@ -80,7 +83,7 @@ public class Player : MonoBehaviour
         }
         thruster.color = thruster_colors[thruster_anim_index];
 
-        accel += new Vector2(hor, vert) * thrust;
+        accel += new Vector2(h, v) * thrust;
         accel += gravity;
         accel *= Time.deltaTime;
 
@@ -135,21 +138,21 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // You died
+        // Player death
         transform.position = Vector3.zero;
         accel = vel = Vector2.zero;
         left_wing_trail.Clear();
         right_wing_trail.Clear();
     }
 
-    void OnGUI()
-    {
-        float hor = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+    // void OnGUI()
+    // {
+    //     float hor = Input.GetAxis("Horizontal");
+    //     float vert = Input.GetAxis("Vertical");
 
-        GUILayout.Box(string.Format("Accel:{0}\nVel:{1}\nPos:{2}", accel, vel, transform.position));
+    //     GUILayout.Box(string.Format("Accel:{0}\nVel:{1}\nPos:{2}", accel, vel, transform.position));
 
-        float trigger = Input.GetAxis("Thrust");
-        GUILayout.Box(string.Format("Input:({0:0.00000}, {1:0.00000})\nThrust:{2:0.0000}", hor, vert, trigger));
-    }
+    //     float trigger = Input.GetAxis("Thrust");
+    //     GUILayout.Box(string.Format("Input:({0:0.00000}, {1:0.00000})\nThrust:{2:0.0000}", hor, vert, trigger));
+    // }
 }
